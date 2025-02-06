76e4c6898c5346508769f53d6456ce1190f0e8f9
pranavbhalla22 <pranav.bhalla@edu.sait.ca>
2025-02-06 13:53:09 -0700

    commit in homeScreen

--- components/homeScreen.tsx
+++ components/homeScreen.tsx
@@ -1,1 +1,29 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import CityScreen from "./cityScreen";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
+const Tab = createBottomTabNavigator();
+
+const HomeScreen: React.FC = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Calgary" options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="location-sharp" color={color} size={size} />
+          ),
+        }}>
+        {() => <CityScreen city="Calgary" url="https://www.calgary.ca/home.html" />}
+      </Tab.Screen>
+      <Tab.Screen name="Edmonton" options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="location-sharp" color={color} size={size} />
+          ),
+        }}>
+        {() => <CityScreen city="Edmonton" url="https://www.edmonton.ca/" />}
+      </Tab.Screen>
+    </Tab.Navigator>
+  );
+};
+
+export default HomeScreen;
